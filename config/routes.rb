8db97613594a7dcad4657aa0dcfcf08088b08360
mattbechtel1 <bechma02@gmail.com>
2020-02-05 14:49:10 -0500

    bugfix on poorly named route

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :stages, only: [:create, :index]
-  resources :beds, only: [:updated]
+  resources :beds, only: [:update]
   resources :fields
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
