5fab3d361921e3e14e0eb96d845483573f4a006d
mattbechtel1 <bechma02@gmail.com>
2020-02-08 16:17:08 -0500

    allow cascade deletion for todos

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,10 +1,21 @@
 class TodosController < ApplicationController
     def create
-        Todo.create(strong_params)
+        todo = Todo.create(strong_params)
+        render json: todo, only: [:id, :note, :due_date, :start_date, :complete, :user_id, :field_id, :bed_id]
+    end
+
+    def show
+        todo = Todo.find(params[:id])
+        render json: todo, only: [:id, :note, :due_date, :start_date, :complete, :user_id, :field_id, :bed_id]
+    end
+
+    def destroy
+        
     end
 
     private
 
     def strong_params
         params.require(:todo).permit(:note, :due_date, :start_date, :user_id, :field_id, :bed_id, :complete)
+    end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ApplicationRecord
-    # has_secure_password :password
+    has_secure_password :password
     validates :username, uniqueness: {case_sensitive: false}
     has_many :fields
     has_many :beds, through: :fields
