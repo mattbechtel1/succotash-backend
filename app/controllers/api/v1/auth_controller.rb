6834b02fb9c9a258490cc6dc9ec728a437156488
mattbechtel1 <bechma02@gmail.com>
2020-02-07 20:14:34 -0500

    trying insecure with no JWT

--- app/controllers/api/v1/auth_controller.rb
+++ app/controllers/api/v1/auth_controller.rb
@@ -6,9 +6,8 @@ class Api::V1::AuthController < ApplicationController
         if user
             if user.authenticate(params[:password])
                 # user found and password authenticated
-                # token = encode({user_id: user.id})
-                # render json: { user: UserSerializer.new(user).to_serialized_json, jwt: token }, status: :accepted
-                render json: {key: secret_key, user_id: user.id}
+                token = encode({user_id: user.id})
+                render json: { user: UserSerializer.new(user).to_serialized_json, jwt: token }, status: :accepted
             else
                 # user found, but bad password
                 render json: { error: true, message: "Password does not match. Please try again." }, status: :unauthorized
