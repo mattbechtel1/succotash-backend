ad3e90a52a956addfea63035729eeb5e3ed0aea4
mattbechtel1 <bechma02@gmail.com>
2020-02-20 14:37:22 -0500

    bugfix allowing insert of new stage completely within existing stage

--- app/controllers/stages_controller.rb
+++ app/controllers/stages_controller.rb
@@ -17,12 +17,29 @@ class StagesController < ApplicationController
             overlapped = bed.stages.where('start_date >= ? and due_date <= ?', new_start_date, new_due_date) 
             overlapped.destroy_all
             
-            # checks for stages that now have another stage inside them and edits their due date
-            needs_end_date_adjustment = bed.stages.detect {|stage| (stage.start_date < new_start_date && !stage.due_date) || (stage.start_date < new_start_date && stage.due_date >= new_due_date) }
-            if needs_end_date_adjustment
-                needs_end_date_adjustment.update(due_date: new_start_date - 1.day)
+            # edits the nil due_date stage if necessary
+            nil_needs_end_date_adjustment = bed.stages.detect {|stage| stage.start_date < new_start_date && !stage.due_date }
+            
+            if nil_needs_end_date_adjustment
+                nil_needs_end_date_adjustment.update(due_date: new_start_date - 1.day)
+            end
+
+            # edits the due_date when new stage overlaps at the end of old stage
+            needs_end_date_pushed_back = bed.stages.detect { |stage| stage.start_date < new_start_date && stage.due_date >= new_start_date && stage.due_date < new_due_date}
+            if needs_end_date_pushed_back
+                needs_end_date_pushed_back.update(due_date: new_start_date - 1.day)
             end
             
+            # clones and edits due_date when new stage is inserted completely into old stage without hitting either end
+            needs_cloning = bed.stages.detect { |stage| stage.start_date < new_start_date && stage.due_date > new_due_date}
+            if needs_cloning
+                cloned_stage = needs_cloning.dup
+                cloned_stage.start_date = new_due_date + 1.day
+                needs_cloning.update(due_date: new_start_date - 1.day)
+
+                cloned_stage.save
+            end
+
             # checks for stages that are now overlapped at their beginning
             start_pushed_back = bed.stages.detect {|stage| stage.start_date >= new_start_date && stage.start_date < new_due_date && ((stage.due_date && stage.due_date > new_due_date) || !stage.due_date) } 
 
