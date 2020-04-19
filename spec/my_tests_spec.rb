bb7521201bb7c256ec6e813be126c309a73ca009
Matt Bechtel <bechma02@gmail.com>
2020-04-19 15:58:51 -0400

    added some field tests

--- spec/my_tests_spec.rb
+++ spec/my_tests_spec.rb
@@ -13,3 +13,28 @@ describe 'get all crops route', :type => :request do
         expect(response).to have_http_status(:success)
     end
 end
+
+describe 'Field is valid when', :type => :model do
+    user = User.new(username: 'Mike')
+    valid_field = Field.new(user: user, name: "#{Faker::ProgrammingLanguage.unique.name} Field")
+    field_without_owner = Field.new(name: 'pipsqueak')
+    field_wihtout_name = Field.new(user: user)
+    new_field = Field.new(user: user, name: 'new')
+
+    it 'it has a valid user and username' do
+        expect(valid_field).to be_valid
+        expect(field_wihtout_name).to be_invalid
+        expect(field_without_owner).to be_invalid
+    end
+
+    it "it's name is not literally 'new'" do
+        expect(new_field).to be_invalid
+    end
+
+    it "it's slug is not duplicative" do
+        slug_field = Field.new(user: user, name: 'my-field')
+        slug_field2 = Field.new(user: user, name: 'my field')
+        expect(slug_field2).to be_invalid
+    end
+
+end
