16ee8c98d439a5ba2e37878f34f5010d2ca7cd57
Matt Bechtel <bechma02@gmail.com>
2020-04-19 15:58:51 -0400

    added some field tests

--- app/models/field.rb
+++ app/models/field.rb
@@ -3,6 +3,7 @@ class Field < ApplicationRecord
     has_many :beds
     has_many :stages, through: :beds
     has_many :todos
+    validates :name, presence: true
     validates :slug, uniqueness: { scope: :user_id, message: "Field name already in use. Please select a different name or delete the existing field."}
     validate :name_not_new
     
--- spec/factories.rb
+++ spec/factories.rb
@@ -3,14 +3,10 @@ FactoryBot.define do
 
     cat_options = [:vegetable, :fruit, :legume, :grain, :flower, :grass, :herb, :other]
     measure_options = [:bushel, :ind_count, :bunch, :peck, :pounds, :crate, :dry_quart]
-    
-    # factory :user, class User do
-    #     username 'Bob'
-    #     password 'pickles'
-    # end
+    dumb_user = User.new(username: 'Bob')
 
     factory :crop, class: Crop do
-        name {[Faker::Food.fruits, Faker::Food.vegetables].sample}
+        name {[Faker::Food.unique.fruits, Faker::Food.unique.vegetables].sample}
         category {cat_options.sample}
         default_measure {measure_options.sample}
     end
