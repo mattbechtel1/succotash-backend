4f18478fce427ef2e3706e6c11289af93bc06c18
Matthew Bechtel <bechma02@gmail.com>
2020-12-05 18:31:05 -0500

    moving email address to ENV

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,7 +39,7 @@ Rails.application.configure do
     address:              'smtp.gmail.com',
     port:                 587,
     # domain:               'example.com',
-    user_name:            'succotashapp',
+    user_name:            ENV["gmail_username"],
     password:             ENV["gmail_password"],
     authentication:       'plain',
     enable_starttls_auto: true  }
