1a1fa413d8d4af22064e26c14602fbf97b9672bf
Matthew Bechtel <bechma02@gmail.com>
2021-01-12 00:44:23 -0500

    password reset email added

--- app/controllers/api/v1/password_controller.rb
+++ app/controllers/api/v1/password_controller.rb
@@ -17,5 +17,32 @@ class Api::V1::PasswordController < ApplicationController
     end
 
     def reset
+        if params[:email].blank?
+            # No email provided
+            render json: { error: "Please provide an e-mail address." }
+        end
+        if params[:username].blank?
+            render json: { error: "Please provide a username." }
+        end
+        if params[:password].blank?
+            render json: { error: "Please provide a password." }
+        end
+        if params[:password] != params[:confirm_password]
+            render json: { error: "Passwords do not match. Please try again."}
+        end
+
+        user = User.find_by(reset_password_token: params[:token])
+        if user.present? && user.password_token_valid?
+            user.update(strong_params)
+            byebug
+            render json: UserSerializer.new(user).to_serialized_json
+        end
+    end
+
+
+    private
+
+    def strong_params
+        params.permit(:password, :email)
     end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
     validates :username, uniqueness: {case_sensitive: false}
     validates_format_of :username, with: /\A[a-zA-Z0-9]*\z/, on: :create, message: 'Username is invalid. Please use only alphanumeric characters.'
     validates_length_of :username, within: 4..25, on: :create
-    validates_length_of :password, within: 6..25, on: [:create, :update]
+    validates_length_of :password, within: 6..25, on: [:create, :update], unless: Proc.new { |a| !a.new_record? && a.password.blank? }
     validates :email, 
         format: { with: /(.+)@(.+)/, message: "Email invalid"  },
         uniqueness: { case_sensitive: false },
@@ -17,12 +17,23 @@ class User < ApplicationRecord
     has_many :crops, through: :favorites
 
     def generate_password_token!
-        self.regenerate_reset_password_token
+        self.reset_password_token = generate_token
         self.reset_password_sent_at = Time.now.utc
-        self.save!
+        byebug
+        save!
     end
 
     def send_dummy_email!
         UserMailer.password_reset_email(self).deliver_now
+    end
+
+    def password_token_valid?
+        (self.reset_password_sent_at + 4.hours) > Time.now.utc
+    end
+
+    private
+    
+    def generate_token
+        SecureRandom.base58(5)
     end
 end
